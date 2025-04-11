6f7d2a6cbba2df56f325bd0186fab93cdcf63d8a
Manoel Lucio <manoelluciodev@gmail.com>
2025-04-10 23:53:35 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className="flex  flex-col items-center justify-center h-screen gap-4">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">Lista de Tarefas</h1>
+        <h2 className="text-lg text-gray-500 ">Crie e gerencie suas tarefas com facilidade</h2>
+
+      </div>
+
+
+      <div className="flex gap-2 p-2 rounded-md border border-gray-300 w-[448px]">
+        <Input placeholder="Adicione uma tarefa" className="outline-none bg-transparent ring-0 border-none shadow-none" />
+        <Button>Adicionar</Button>
+      </div>
+
     </div>
   );
 }
