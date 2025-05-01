5688bfcdeec3a49a4cf8677b37af2cff16a65b32
Manoel Lucio <manoelluciodev@gmail.com>
2025-05-01 11:21:32 -0300

    Update dependencies and enhance UI components. Added new packages for UI elements including Radix UI components, class variance authority, and animation support. Improved styling in globals.css and updated the main page layout with input and tab components for better task management.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Tabs, TabsTrigger, TabsList, TabsContent } from "@/components/ui/tabs";
 
 export default function Home() {
+
+  
+
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Lista de Tarefas</h1>
+      <h2 className="text-lg text-gray-500">Organize suas tarefas de forma simples e elegante</h2>
+      <div className="flex  items-center bg-white rounded-md p-2 w-[28rem] gap-2 mt-4">
+        <Input placeholder="Adicione uma tarefa" />
+        <Button>
+          <Plus />
+        </Button>
+      </div>
+      <div className="mt-4">
+        <Tabs defaultValue="all" className="w-[400px] bg-white rounded-md p-2">
+          <TabsList className="w-full  grid-cols-2">
+          <TabsTrigger value="all">Todas</TabsTrigger>
+          <TabsTrigger value="favorite">Favoritas</TabsTrigger>
+          </TabsList>
+          <TabsContent value="all">
+            <div>
+              <h1>Todas</h1>
+            </div>
+          </TabsContent>
+          <TabsContent value="favorite">
+            <div>
+              <h1>Favoritas</h1>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+
     </div>
   );
 }
